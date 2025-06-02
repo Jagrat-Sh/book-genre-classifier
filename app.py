6adb457e94df48b3a5f34851f75b346e15fc6ae5
Jagrat Shrivastava <jagrat.shrivastava01@gmail.com>
2025-06-02 20:48:23 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from transformers import pipeline
+
+app = Flask(__name__)
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+CANDIDATE_GENRES = [
+    "science fiction", "romance", "thriller", "fantasy",
+    "historical", "non-fiction", "mystery", "horror", "young adult"
+]
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    if request.method == "POST":
+        blurb = request.form["blurb"]
+        prediction = classifier(blurb, CANDIDATE_GENRES)
+        result = list(zip(prediction["labels"], prediction["scores"]))
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
